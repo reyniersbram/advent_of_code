c957e2a8ff7904da985978012141ffdb100d33b5
reyniersbram <reyniersbram@gmail.com>
2022-12-03 12:09:22 +0100

    day 3

--- Day_3/solution.py
+++ Day_3/solution.py
@@ -6,12 +6,19 @@ def file_to_list(file):
 
 def part1(file):
     lijst = file_to_list(file)
-    return "Not solved yet"
+    common_items = ((set(line[:len(line) // 2]) & set(line[len(line) // 2:])).pop() for line in lijst)
+    priority_sum = sum(ord(item) - (ord('a') - 1) if item.islower() else ord(item) - (ord('A') - 27) for item in
+                       common_items)
+    return priority_sum
 
 
 def part2(file):
     lijst = file_to_list(file)
-    return "Not solved yet"
+    groups = (lijst[i: i + 3] for i in range(0, len(lijst), 3))
+    common_items = ((set(group[0]) & set(group[1]) & set(group[2])).pop() for group in groups)
+    priority_sum = sum(ord(item) - (ord('a') - 1) if item.islower() else ord(item) - (ord('A') - 27) for item in
+                       common_items)
+    return priority_sum
 
 
 if __name__ == '__main__':
