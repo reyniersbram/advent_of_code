65ebddb3e54bbd951882c8c7be8f1fff35e3cbf9
reyniersbram <reyniersbram@gmail.com>
2022-12-04 12:55:13 +0100

    day 4

--- Day_4/solution.py
+++ Day_4/solution.py
@@ -4,14 +4,25 @@ def file_to_list(file):
     return lijst
 
 
+def to_set(*start_end):
+    return set(range(*start_end))
+
+
 def part1(file):
     lijst = file_to_list(file)
-    return "Not solved yet"
+    sections = ((tuple(int(x) for x in section.split('-')) for section in line.split(',')) for line in lijst)
+    sections = (tuple(to_set(section[0], section[1] + 1) for section in pair) for pair in sections)
+    sections_overlap = ((pair[0] | pair[1] == pair[0] or pair[0] | pair[1] == pair[1]) for pair in sections)
+
+    return sum(sections_overlap)
 
 
 def part2(file):
     lijst = file_to_list(file)
-    return "Not solved yet"
+    sections = ((tuple(int(x) for x in section.split('-')) for section in line.split(',')) for line in lijst)
+    sections = (tuple(to_set(section[0], section[1] + 1) for section in pair) for pair in sections)
+    sections_overlap = (len(pair[0] & pair[1]) != 0 for pair in sections)
+    return sum(sections_overlap)
 
 
 if __name__ == '__main__':
