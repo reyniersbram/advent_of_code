57ff2235ca022bdea2d2c5ca8eb15c113631e80e
reyniersbram <reyniersbram@gmail.com>
2022-12-05 11:41:25 +0100

    use file_reader file for day 5

--- Day_5/solution.py
+++ Day_5/solution.py
@@ -1,7 +1,4 @@
-def file_to_list(file):
-    with open(file, "r") as text_wrapper:
-        lijst = [line.replace("\n", "") for line in text_wrapper]
-    return lijst
+from file_reader import file_to_list
 
 
 class Stack:
