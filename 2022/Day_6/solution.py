ec689bd658cd19589473a6b1419f8732a7250d40
reyniersbram <reyniersbram@gmail.com>
2022-12-06 09:26:06 +0100

    Day 6

--- 2022/Day_6/solution.py
+++ 2022/Day_6/solution.py
@@ -1,14 +1,27 @@
 from file_reader import file_to_list
 
 
+def is_marker(buffer: str, length: int) -> bool:
+    return len(buffer) == length and len(set(buffer)) == length
+
+
+def find_marker(input_stream: str, marker_length: int) -> int:
+    buffer = input_stream[:marker_length]
+    i = marker_length
+    while not is_marker(buffer, marker_length):
+        buffer = buffer[1::] + input_stream[i]
+        i += 1
+    return i
+
+
 def part1(file):
     lijst = file_to_list(file)
-    return "Not solved yet"
+    return find_marker(lijst[0], 4)
 
 
 def part2(file):
     lijst = file_to_list(file)
-    return "Not solved yet"
+    return find_marker(lijst[0], 14)
 
 
 if __name__ == '__main__':
