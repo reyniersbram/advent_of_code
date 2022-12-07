ce70a9c048e4b86d7d7ba8bef1a8ea336ca5cf87
reyniersbram <reyniersbram@gmail.com>
2022-12-07 12:33:13 +0100

    update setup command

--- setup.sh
+++ setup.sh
@@ -1,15 +1,39 @@
 #! /bin/bash
 
+syntaxError() {
+	echo "Syntax Error: ./setup.sh [-y <directory>]" 1>&2
+	exit 1
+}
+
+argumentError () {
+	echo "Argument Error: Expected an argument, but none was given" 1>&2
+	exit 2
+}
+
+year=$(date -u +"%Y")
+while getopts ":y:" opt
+do
+	case $opt in
+		y)	year=$OPTARG
+			;;
+		\?)	syntaxError
+			;;
+		\:)	argumentError
+	esac
+done
+shift $((OPTIND - 1))
+
 # Create directories
-mkdir -v "$1/Day_"{1..24}
+mkdir $year
+mkdir -v "$year/Day_"{1..24}
 
 # Create input files
-touch "$1/Day_"{1..24}/input.txt
+touch "$year/Day_"{1..24}/input.txt
 for i in {1..24}
 do
-	echo "created file $1/Day_$i/input.txt"
+	echo "created file $year/Day_$i/input.txt"
 done
 
 # Copy formats
-echo "$1/Day_"{1..24} | xargs -n 1 cp -v solution.py 
+echo "$year/Day_"{1..24}/solution.py | xargs -n 1 cp -v template.py 
 
