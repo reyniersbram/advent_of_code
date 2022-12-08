c9f6a98a56daa79acb083b48c47b5acd5b26a8d3
reyniersbram <reyniersbram@gmail.com>
2022-12-08 10:59:51 +0100

    update template to future das

--- 2022/Day_15/solution.py
+++ 2022/Day_15/solution.py
@@ -1,17 +1,14 @@
-from typing import Generator
-
-
-def part1(lines: Generator[str, None, None]):
+def part1(lines: list[str]):
     return "Not solved yet"
 
 
-def part2(lines: Generator[str, None, None]):
+def part2(lines: list[str]):
     return "Not solved yet"
 
 
 if __name__ == '__main__':
     file_name = "input.txt"
-    with open(file_name,'r') as text_wrapper:  
-        lines = (line.replace("\n", "") for line in text_wrapper)
-    print(f'Answer 1 is: {part1(lines)}')
-    print(f'Answer 2 is: {part2(lines)}')
+    with open(file_name, 'r') as text_wrapper:
+        file_lines = [line.replace("\n", "") for line in text_wrapper]
+        print(f'Answer 1 is: {part1(file_lines)}')
+        print(f'Answer 2 is: {part2(file_lines)}')
