778e46b8c537e760014f93be0337e49487b8e27e
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-12-02 19:44:22 +0100

    solve 2023 day 2

---
+++ 2023/day2/solution2.py
@@ -0,0 +1,79 @@
+#! /usr/bin/env python3
+
+from __future__ import annotations
+import re
+import os
+import sys
+script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+
+class CubeSet:
+    def __init__(self, red: int, green: int, blue: int) -> None:
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+    def __str__(self) -> str:
+        return f"red: {self.red}, green: {self.green}, blue: {self.blue}"
+
+    def is_possible(self, other: CubeSet) -> bool:
+        return self.red <= other.red and self.green <= other.green and self.blue <= other.blue
+
+    @property
+    def power(self) -> int:
+        return self.red * self.green * self.blue
+    
+
+class Game:
+    def __init__(self, line: str) -> None:
+        line = line.replace(" ", "")
+        game_id_string, revealed_cubes_string = line.split(":")
+        cube_set_strings = revealed_cubes_string.split(";")
+
+        cube_sets = []
+        for cube_set_string in cube_set_strings:
+            pairs = re.findall(r"(\d+)([a-z]+)", cube_set_string)
+            colors = {pair[1]: int(pair[0]) for pair in pairs}
+            cube_sets.append(CubeSet(colors.get("red", 0), colors.get("green", 0), colors.get("blue", 0)))
+
+        self.id = int(game_id_string.replace("Game", ""))
+        self.cube_sets = cube_sets
+
+    def __str__(self) -> str:
+        return f"Game {self.id}: \n{[str(cube_set) for cube_set in self.cube_sets]}"
+
+    def is_possible(self, other_cube_set: CubeSet) -> bool:
+        return all([cube_set.is_possible(other_cube_set) for cube_set in self.cube_sets])
+
+    def get_fewest_cubes(self) -> CubeSet:
+        min_cubes = (0, 0, 0)
+        for cube_set in self.cube_sets:
+            min_cubes = (
+                max(min_cubes[0], cube_set.red),
+                max(min_cubes[1], cube_set.green),
+                max(min_cubes[2], cube_set.blue),
+            )
+        return CubeSet(*min_cubes)
+
+def solve(lines: list[str]) -> int:
+    fewest_power_sum = 0
+    for line in lines:
+        game = Game(line)
+        fewest_power_sum += game.get_fewest_cubes().power
+    return fewest_power_sum
+
+
+def main(file_name: str):
+    """
+    >>> main(f"{script_directory}/test.txt")
+    2286
+    """
+    with open(file_name, 'r') as file:
+        lines = [line.replace("\n", "") for line in file]
+        print(solve(lines))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main(f"{script_directory}/input.txt")
